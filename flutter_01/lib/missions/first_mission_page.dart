6d4efbd2a6af598d67f345c08924af4d96e8b99c
dlwogmlzz <lwwljh90@gmail.com>
2026-07-28 11:42:18 +0900

    20260728 Flutter Study # Day23 - Widget함수와 Widget생성 & 리펙토링후(Extract Widget) 차이 비교

--- flutter_01/lib/missions/first_mission_page.dart
+++ flutter_01/lib/missions/first_mission_page.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class FirstMissionPage extends StatelessWidget {
   const FirstMissionPage({super.key});
 
+  final String buttontitle = "ユーチューブショッピングへ移動!";
+
+  final String imgSrc =
+      "https://images.unsplash.com/photo-1785038278893-6e708804c1be?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D";
+  final String title = "タイトル";
+  final String desc = "説明です。";
+
+  static final Widget missionTitle = Text(
+    '最新の提携先メリット',
+    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +29,10 @@ class FirstMissionPage extends StatelessWidget {
               spacing: 10,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                _getTitle(),
-                _getBanner(),
-                _getBanner(),
-                _getButton(title: "ユーチューブショッピングへ移動!"),
+                missionTitle,
+                BannerItem(imgSrc: imgSrc, title: title, desc: desc),
+                BannerItem(imgSrc: imgSrc, title: title, desc: desc),
+                AdsButton(title: buttontitle),
               ],
             ),
           ),
@@ -28,62 +40,70 @@ class FirstMissionPage extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _getTitle() {
-  return Text(
-    '最新の提携先メリット',
-    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  static final Widget missionBox = Container(
+    height: 100,
+    color: Colors.lightGreenAccent,
   );
-}
-
-Widget _getBox({double height = 50}) {
-  return Container(height: 100, color: Colors.lightGreenAccent);
-}
-
-Widget _getButton({required String title}) {
-  var btnStyle = TextButton.styleFrom(
-    foregroundColor: Colors.white,
-    backgroundColor: Colors.grey,
-  );
-
-  return TextButton(
-    style: btnStyle,
-    onPressed: () {
-      print('버튼 클릭됨 :$title');
-    },
-    child: Text(title),
-  );
-}
-
-Widget _getBanner({
   // required String imgSrc = "https://images.unsplash.com/photo-1785038278893-6e708804c1be?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
   // required String title,
   // required String desc,
   // 기본 값이 있으면 required를 빼야됨.
-  String imgSrc =
-      "https://images.unsplash.com/photo-1785038278893-6e708804c1be?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D",
-  String title = "제목",
-  String desc = "설명입니다.",
-}) {
-  return Row(
-    spacing: 10,
-    children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Image.network(
-          height: 100,
-          width: 100,
-          imgSrc,
-          fit: BoxFit.cover,
+}
+
+class AdsButton extends StatelessWidget {
+  const AdsButton({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.grey,
+      ),
+      onPressed: () {
+        print('버튼 클릭됨 :$title');
+      },
+      child: Text(title),
+    );
+  }
+}
+
+class BannerItem extends StatelessWidget {
+  const BannerItem({
+    super.key,
+    required this.imgSrc,
+    required this.title,
+    required this.desc,
+  });
+
+  final String imgSrc;
+  final String title;
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 10,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.network(
+            height: 100,
+            width: 100,
+            imgSrc,
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Text(title), Text(desc)],
-      ),
-    ],
-  );
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text(title), Text(desc)],
+        ),
+      ],
+    );
+  }
 }
 
 // ClipRRect (
